67c7ecc8a723691802d9e7a83eae57bbe265415e
dunjia <13433126742@163.com>
2024-04-04 22:24:32 +0800

    update balanced-tree main.cpp

--- balanced-tree/main.cpp
+++ balanced-tree/main.cpp
@@ -17,10 +17,13 @@ public:
     int height(TreeNode* root)
     {
         if(!root) return 0;
+        // 返回最深高度 同时继续向下 故加1
         else return max(height(root->left), height(root->right)) + 1;
     }
     bool isBalanced(TreeNode* root) {
         if(!root) return true;
+        // 保证左子树和右子树平衡
+        // 保证左右子树高度不超过1
         else return abs(height(root->left) - height(root->right)) <= 1 && isBalanced(root->left) && isBalanced(root->right);
     }
 };
@@ -34,14 +37,17 @@ public:
         }
         int leftHeight = height(root->left);
         int rightHeight = height(root->right);
+        // 当前节点左右子树是否差值不超过1 或者是否子树中存在不平衡的情况(存在-1值)
         if (leftHeight == -1 || rightHeight == -1 || abs(leftHeight - rightHeight) > 1) {
             return -1;
         } else {
+            // 继续上探 高度加1
             return max(leftHeight, rightHeight) + 1;
         }
     }
 
     bool isBalanced(TreeNode* root) {
+        // 用0或-1来判断平衡
         return height(root) >= 0;
     }
 };
