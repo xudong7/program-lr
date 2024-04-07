16b6baf43ede19a3fffcf2a212dc1cabd2483e19
xudong7 <13433126742@163.com>
2024-04-07 18:01:12 +0800

    update class_new file

---
+++ class_new/main.cpp
@@ -0,0 +1,55 @@
+# include<iostream>
+# include<bits/stdc++.h>
+using namespace std;
+
+class Student
+{
+private:
+    int data;
+public:
+    Student()
+    {
+        data = 0;
+        cout << "default" << endl;
+    }
+    Student(int n) : data(n)
+    {
+        cout << "value" << endl;
+    }
+    Student(const Student& other) : data(other.data)
+    {
+        cout << "copy" << endl;
+    }
+    ~Student()
+    {
+        cout << "~Student(): " <<  endl;
+    }
+};
+
+Student& func(Student& a)
+{
+    return a;
+}
+
+void func2(Student a)
+{
+    // 不返回类对象 则函数结束立刻析构
+    cout << "func2" << endl;
+}
+
+Student func3()
+{
+    // 返回类对象 之前是立刻析构 由于编译优化 现在会在main函数结束后才析构
+    Student c(2);
+    return c;
+}
+
+int main()
+{
+    Student s(20);
+    Student ret(func(s));
+    func2(s);
+    Student obj(func3());
+    cout << "end" << endl;
+    return 0;
+}
